f1dc7633b74702a0aad24bae4afeb5722ba98efe
E0SelmY4V-PC <2507917746@qq.com>
2022-12-05 18:51:59 +0800

    add: 声明

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import { ChildProcessWithoutNullStreams } from 'child_process'
+import { TransformOptions } from '@babel/core'
+declare type Conf = {
+	opts: TransformOptions,
+	out: string,
+}
+export let defConf: Conf
+export function iePasser(
+	path: string,
+	test: { [testName: number | string]: () => void },
+	conf: Conf,
+): Promise<ChildProcessWithoutNullStreams>
+export default iePasser
